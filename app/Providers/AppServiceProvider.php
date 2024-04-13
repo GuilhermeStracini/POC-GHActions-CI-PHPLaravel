25a8b45efa1fe713dca1b7c30ace5689e3a39e5d
Guilherme Branco Stracini <guilhermes@outsurance.ie>
2024-04-13 17:46:05 +0100

    Fix message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,7 +9,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register()::
+    public function register():
     void
     {
         //
